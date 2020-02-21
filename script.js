f5c1616caf814da386110a7b65af218a8a62d759
Jonathan-SS <56107465+Jonathan-SS@users.noreply.github.com>
2020-02-21 12:21:18 +0100

    version 2.2.1 21022020 jonathan

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // JavaScript Document
+
+function gaaFremad() {
+    if (billedIndeks < billedliste.length - 1) {
+        billedIndeks++;
+    } else {
+        billedIndeks = 0;
+    }
+
+    document.getElementById("slidebillede").src = billedliste[billedIndeks];
+}
+
+function gaaTilbage() {
+    if (billedIndeks > 0) {
+        billedIndeks--;
+    } else {
+        billedIndeks = 2;
+    }
+
+    document.getElementById("slidebillede").src = billedliste[billedIndeks];
+}
+
+// ------- Hovedprogram ---------
+const billedliste = ["images/slide1.jpg", "images/slide2.jpg", "loevepar.jpg"];
+let billedIndeks = 0;
+
+document.getElementById("frem").addEventListener("click", function () {
+    gaaFremad();
+})
+
+document.getElementById("tilbage").addEventListener("click", function () {
+    gaaTilbage();
+})
